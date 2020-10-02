08a9b316b778129794f2ef6ddac189fbdcbaa011
sergey42osn <sergey42osn@gmail.com>
2020-10-02 13:44:58 +0700

    3 com

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('add-users-component', require('./components/AddButtonUsers.vue').default);
+Vue.component('side-bar-menu-component', require('./components/SidebarMenu.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
